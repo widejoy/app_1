a20c2ae1e581bef11f9d13b8f0a3056c73a5d425
widejoy <rogerantony40@ug.cusat.ac.in>
2023-05-10 16:09:04 +0530

    arranged the main body in a class

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,32 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Color.fromARGB(255, 212, 59, 59), Colors.black,]),
-          ),
-          child: const Center(
-            child: Text('hello world'),
-          ),
-        ),
+        body:BaseContainer(),
       ),
     ),
   );
+}
+
+class BaseContainer extends StatelessWidget{
+  @override
+  Widget build(context) {
+    return Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 120, 69, 221),
+              Colors.black,
+            ],begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+
+            ),
+          ),
+          child: const Center(
+            child: Text(
+              'hello world',
+              style: TextStyle(color: Colors.brown, fontSize: 28),
+            ),
+          ),
+        );
+  }
+
 }
